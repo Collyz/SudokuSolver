59abc7ffad434f6a72f2e5799fbf5b0d4786ff2f
Mohammed Mowla <m.mowla03151@gmail.com>
2023-03-26 22:12:56 -0400

    Almost all constraints can be obtained, not fully complete yet

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -1,13 +1,10 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class SudokuSolver {
 
-    private int[][] puzzle;
-    private final int[] AVAILABILITY_SET = {1,2,3,4,5,6,7,9};
+    private final int[][] puzzle;
+    private final int[] AVAILABILITY_SET = {1,2,3,4,5,6,7,8,9};
 
     public SudokuSolver(){
         this.puzzle = new int[9][9];
@@ -16,7 +13,7 @@ public class SudokuSolver {
     /**
      * Prompts the user to enter the filename to get the puzzle
      */
-    public void getPuzzle(){
+    public void inputPuzzle(){
         try {
             //Getting filename
             Scanner userInput = new Scanner(System.in);
@@ -48,92 +45,133 @@ public class SudokuSolver {
      * Prints the puzzle
      */
     public void printPuzzle(){
-        for(int i = 0; i < puzzle.length;i++){
+        for (int[] ints : puzzle) {
             String row = "";
-            for(int j = 0; j < puzzle[i].length; j++){
-                row+= " " + puzzle[i][j];
+            for (int anInt : ints) {
+                row += " " + anInt;
             }
             row += "\n";
             System.out.print(row);
         }
     }
 
+    public int[][] getPuzzle(){
+        return this.puzzle;
+    }
+
     public int[] getConstraints(int var, int rowPos, int colPos){
-        int[] constraint = AVAILABILITY_SET;
-        int[] horizontal = new int[9];
-        int[] vertical = new int[9];
-        if(var != 0){
+        Set<Integer> set = new HashSet<>();
+        if(var == 0){
             // Get horizontal constraint
-            for(int i = 0; i < 9 ;i++){
-                horizontal[i] = puzzle[rowPos][i];
+            for(int i = 0; i < 9; i++){
+                if(puzzle[rowPos][i] != 0){
+                    set.add(puzzle[rowPos][i]);
+                }
             }
             // Get vertical constraint
             for (int i = 0; i < 9; i++) {
-                vertical[i] = puzzle[i][colPos];
-            }
-
-            // Combine horizontal and vertical constraints
-            List<Integer> constraintList = new ArrayList<>();
-            for (int value : horizontal) {
-                if (value != 0) {
-                    constraintList.add(value);
-                }
-            }
-            for (int value : vertical) {
-                if (value != 0) {
-                    constraintList.add(value);
+                if(puzzle[i][colPos] != 0){
+                    set.add(puzzle[i][colPos]);
                 }
             }
 
-            // Convert constraint list to array and return
-            int[] constraints = new int[constraintList.size()];
-            for (int i = 0; i < constraintList.size(); i++) {
-                constraints[i] = constraintList.get(i);
+            int[] result = new int[set.size()];
+            int i = 0;
+            for (int val : set) {
+                result[i++] = val;
             }
-            return constraints;
+            result = removeAvailable(result);
+            return result;
         }else{
             return null;
         }
     }
 
     public int[] getBoxConstraint(int var, int rowPos, int colPos){
-        int[] constraint = AVAILABILITY_SET;
         if(var != 0){
-            int boxRow = rowPos / 3; // integer division to get box row index (0-2)
-            int boxCol = colPos / 3; // integer division to get box column index (0-2)
-            int startRow = boxRow * 3; // calculate start row index of the box
-            int startCol = boxCol * 3; // calculate start column index of the box
-            for(int i = startRow; i < startRow + 3; i++){
-                for(int j = startCol; j < startCol + 3; j++){
-                    if(puzzle[i][j] != 0){
-                        constraint = removeValue(constraint, puzzle[i][j]);
-                    }
-                }
-            }
-        }else{
+            System.out.println("null");
             return null;
         }
-        return constraint;
-    }
-
-    private int[] removeValue(int[] array, int value){
-        int j = 0;
-        for(int i = 0; i < array.length; i++){
-            if(array[i] != value){
-                array[j++] = array[i];
+        int boxRow = rowPos / 3; // integer division to get box row index (0-2)
+        int boxCol = colPos / 3; // integer division to get box column index (0-2)
+        int startRow = boxRow * 3; // calculate start row index of the box
+        int startCol = boxCol * 3; // calculate start column index of the box
+        ArrayList<Integer> subArray = new ArrayList<>();
+        for(int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if(var == puzzle[startRow + i][startCol + j]){
+                    subArray.add(0);
+                }else {
+                    subArray.add(this.puzzle[startRow + i][startCol + j]);
+                }
             }
         }
-        int[] result = new int[j];
-        System.arraycopy(array, 0, result, 0, j);
+        int[] result = new int[subArray.size()];
+        for(int i = 0; i < subArray.size();i++) {
+            result[i] = subArray.get(i);
+        }
+        result = removeAvailable(result);
         return result;
     }
 
     public void getAllConstraints(){
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
-                System.out.println(Arrays.toString(getConstraints(puzzle[i][j], i, j)));
+                Set<Integer> combined = new HashSet<>();
+                int[] temp1 = getBoxConstraint(puzzle[i][j], i, j);
+                int[] temp2 = getConstraints(puzzle[i][j], i, j);
+                if(temp1 != null) {
+                    for (int k = 0; k < temp1.length; k++) {
+                        combined.add(temp1[k]);
+                    }
+                }
+                if(temp2 != null){
+                    for(int k = 0; k < temp2.length; k++){
+                        combined.add(temp2[k]);
+                    }
+                }
+
+                int [] result = new int[combined.size()];
+                int k = 0;
+
+                for (int val : combined) {
+                    result[k++] = val;
+                }
+                result = removeAvailable(result);
+                System.out.println("Puzzle pos: " + i + "," + j + ":"
+                        + Arrays.toString(result));
             }
         }
     }
+
+
+
+    /**
+     * Sorts and removes repetitive values in the constraints
+     * @param array The array to sort and remove repeating values
+     * @return The sorted and unique array
+     */
+    public int[] removeAvailable(int[] array) {
+        List<Integer> list1 = new ArrayList<>();
+        for (int i : AVAILABILITY_SET) {
+            list1.add(i);
+        }
+
+        List<Integer> list2 = new ArrayList<>();
+        for (int i : array) {
+            list2.add(i);
+        }
+
+        list1.removeAll(list2);
+        int[] result = new int[list1.size()];
+        for(int i = 0; i < list1.size();i++){
+            result[i] = list1.get(i);
+        }
+
+        Arrays.sort(result);
+        return result;
+    }
+
+
 
 }
